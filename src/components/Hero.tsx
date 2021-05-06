e0f928d8817d5fd1ec901b319715d3144555211c
Fabian <13519140@std.stei.itb.ac.id>
2021-05-06 14:15:54 +0700

    finish welcome page

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import "../styles/Hero.css";
+import { Link } from 'react-router-dom';
 
 const Hero : React.FC = () => {
   return (
     <div id="hero-container">
       <div id="content-container">
         <h1>Thoughtful stationery that sparks creativity</h1>
+        <button type="button" >
+          <Link to="/personal" style={{ textDecoration: 'none' , width: "100%"}}>
+            <b>Register</b>
+          </Link>
+        </button>
       </div>
       <div id="image-container">
-
+        <img src="hero.png" alt="Stationery Image" />
       </div>
-      
     </div>
   )
 };
