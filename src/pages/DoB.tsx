0e205ad9bf586cb13acece8078d00eaf0840bfbc
Fabian <13519140@std.stei.itb.ac.id>
2021-05-06 20:55:51 +0700

    save personal details to session storage

--- src/pages/DoB.tsx
+++ src/pages/DoB.tsx
@@ -4,6 +4,7 @@ import Layout from '../components/Layout';
 import InputField from '../components/InputField';
 import FormLayout from '../components/FormLayout';
 import FilledButton from "../components/FilledButton";
+import "../styles/Personal.css";
 
 const DoB : React.FC = () => {
   const [date, setDate] = useState("");
@@ -12,33 +13,40 @@ const DoB : React.FC = () => {
 
   const handleSubmit = () => {
     console.log(date);
-    history.push("/agreement");
+    if (date !== null) {
+      history.push("/agreement");
+    }
   }
 
   return (
-    <Layout title="Date Of Birth" background="#1E96FC">
-      <FormLayout title="Enter Date of Birth">
-        <form onSubmit={(e) => {
-          e.preventDefault();
-          handleSubmit();
-        }}>
-          <InputField
-            name="Date Of Birth"
-            type="date"
-            value={date}
-            setValue={setDate}
-            placeholder="John"
-          />
-          <FilledButton 
-            name="Next"
-            submit={true}
-            handleClick={handleSubmit}
-            background="black"
-            width="100%"
-            color="white"
-          />
-        </form>
-      </FormLayout>
+    <Layout title="Date Of Birth" background="#81F0E5">
+      <div className="content-container">
+        <FormLayout title="Enter Date of Birth">
+          <form onSubmit={(e) => {
+            e.preventDefault();
+            handleSubmit();
+          }}>
+            <InputField
+              name="Date Of Birth"
+              type="date"
+              value={date}
+              setValue={setDate}
+              placeholder="John"
+            />
+            <FilledButton 
+              name="Next"
+              submit={true}
+              handleClick={handleSubmit}
+              background="black"
+              width="100%"
+              color="white"
+            />
+          </form>
+        </FormLayout>
+        <div className="image-container">
+          <img src="calender.png" alt="Personal" />
+        </div>
+      </div>
     </Layout>
   )
 };
--- src/pages/Personal.tsx
+++ src/pages/Personal.tsx
@@ -1,65 +1,91 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useHistory } from "react-router-dom";
 import Layout from '../components/Layout';
 import InputField from '../components/InputField';
 import FormLayout from '../components/FormLayout';
 import FilledButton from "../components/FilledButton";
+import "../styles/Personal.css";
 
 const Personal : React.FC = () => {
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [firstName, setFirstName] = useState(sessionStorage.getItem("firstName") || "");
+  const [lastName, setLastName] = useState(sessionStorage.getItem("lastName") || "");
+  const [email, setEmail] = useState(sessionStorage.getItem("email") || "");
+  const [password, setPassword] = useState(sessionStorage.getItem("password") || "");
 
   const history = useHistory();
 
+  // Set data to session storage everytime it changes
+  useEffect(() => {
+    sessionStorage.setItem("firstName", firstName);
+  }, [firstName]);
+
+  useEffect(() => {
+    sessionStorage.setItem("lastName", lastName);
+  }, [lastName]);
+
+  useEffect(() => {
+    sessionStorage.setItem("email", email);
+  }, [email]);
+
+  useEffect(() => {
+    sessionStorage.setItem("password", password);
+  }, [password]);
+
   const handleSubmit = () => {
-    console.log(firstName, lastName, email, password);
-    history.push("/dob");
+    if (firstName !== "" && lastName !== "" && email !== "" && password !== "") {
+      history.push("/dob");
+    } else {
+      console.log("isi form")
+    }
   }
 
   return (
     <Layout title="Personal Details" background="#FCE694">
-      <FormLayout title="Personal Details">
-        <form onSubmit={(e) => {
-          e.preventDefault();
-          handleSubmit();
-        }}>
-          <InputField
-            name="First Name"
-            value={firstName}
-            setValue={setFirstName}
-            placeholder="John"
-          />
-          <InputField
-            name="Last Name"
-            value={lastName}
-            setValue={setLastName}
-            placeholder="Doe"
-          />
-          <InputField
-            name="Email"
-            value={email}
-            setValue={setEmail}
-            placeholder="johndoe@email.com"
-          />
-          <InputField
-            name="Password"
-            type="password"
-            value={password}
-            setValue={setPassword}
-            placeholder="***********"
-          />
-          <FilledButton 
-            name="Next"
-            submit={true}
-            handleClick={handleSubmit}
-            background="black"
-            width="100%"
-            color="white"
-          />
-        </form>
-      </FormLayout>
+      <div className="content-container">
+        <FormLayout title="Enter Personal Details">
+          <form onSubmit={(e) => {
+            e.preventDefault();
+            handleSubmit();
+          }}>
+            <InputField
+              name="First Name"
+              value={firstName}
+              setValue={setFirstName}
+              placeholder="John"
+            />
+            <InputField
+              name="Last Name"
+              value={lastName}
+              setValue={setLastName}
+              placeholder="Doe"
+            />
+            <InputField
+              name="Email"
+              value={email}
+              setValue={setEmail}
+              placeholder="johndoe@email.com"
+            />
+            <InputField
+              name="Password"
+              type="password"
+              value={password}
+              setValue={setPassword}
+              placeholder="***********"
+            />
+            <FilledButton 
+              name="Next"
+              submit={true}
+              handleClick={handleSubmit}
+              background="black"
+              width="100%"
+              color="white"
+            />
+          </form>
+        </FormLayout>
+        <div className="image-container">
+          <img src="people.png" alt="Personal" />
+        </div>
+      </div>
     </Layout>
   )
 };
